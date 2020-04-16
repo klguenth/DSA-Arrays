34313a255f1aa4a98737285875cc93d79da721ac
klguenth <klguenth@gmail.com>
2020-04-16 15:15:51 -0500

    update

---
+++ main.js
@@ -0,0 +1,16 @@
+const Array = require('./arrays.js');
+
+function main(){
+
+    Array.SIZE_RATIO = 3;
+
+    // Create an instance of the Array class
+    let arr = new Array();
+
+    // Add an item to the array
+    arr.push(3);
+
+    console.log(arr);
+}
+
+console.log(main(5));
